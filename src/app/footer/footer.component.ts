857473cccd5d5b6fbc54653e1d919dc5da941bfe
krostof <krostof4@wp.pl>
2024-10-07 22:49:40 +0200

    component footer added

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
